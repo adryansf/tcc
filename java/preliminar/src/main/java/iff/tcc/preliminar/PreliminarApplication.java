87a906aa4e2fb63329efedcc78e86f4b7bd53183
Matheus Peixoto <mmpeixoto5@gmail.com>
2025-02-04 17:26:29 -0300

    added java project

---
+++ java/preliminar/src/main/java/iff/tcc/preliminar/PreliminarApplication.java
@@ -0,0 +1,13 @@
+package iff.tcc.preliminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreliminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreliminarApplication.class, args);
+	}
+
+}
---
+++ java/preliminar/src/test/java/iff/tcc/preliminar/PreliminarApplicationTests.java
@@ -0,0 +1,13 @@
+package iff.tcc.preliminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreliminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
